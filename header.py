1c8de7c8676359e20c71bc6d94208e3a0849ad70
Learning for learning <1148995537@qq.com>
2021-06-02 20:51:39 +0800

    add header.py -> change request header 🔥

---
+++ header.py
@@ -0,0 +1,13 @@
+# _*_ coding: utf-8 _*_
+"""
+FGSurfing.api
+~~~~~~~~~~~~
+Use this module to replace the request headers
+:copyright: (c) 2021 by Kris
+"""
+
+
+class Header(object):
+    """Replace Request Headers"""
+    def __init__(self, data: bytes):
+      pass
