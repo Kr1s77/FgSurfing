228c309d33e26e5c400e56965311e92634d0957d
Learning for learning <1148995537@qq.com>
2021-06-03 14:12:36 +0800

    add network.py -> network control 🐼

---
+++ network.py
@@ -0,0 +1,18 @@
+# _*_ coding: utf-8 _*_
+import socket
+
+
+class Network(object):
+    def check_local_port(self, port: int):
+        port_msg = dict()
+        port_msg['host'] = 'localhost'
+        port_msg['port'] = port
+        return self._check_tcp_port(port_msg)
+
+    @staticmethod
+    def _check_tcp_port(port_msg: dict, timeout=2):
+        cs = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        address = (str(port_msg["host"]), int(port_msg["port"]))
+        cs.settimeout(timeout)
+        status = cs.connect_ex(address)
+        return True if status == 0 else False
