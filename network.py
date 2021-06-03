6ebbe5fcfbd2d8505cda2b618f4bb5d0a2dadabd
Learning for learning <1148995537@qq.com>
2021-06-03 14:14:17 +0800

    update network -> add doc 🌟

--- network.py
+++ network.py
@@ -1,4 +1,10 @@
 # _*_ coding: utf-8 _*_
+"""
+FgSurfing.api
+~~~~~~~~~~~~
+Use this module to replace the request headers
+:copyright: (c) 2021 by Kris
+"""
 import socket
 
 
@@ -16,3 +22,7 @@ class Network(object):
         cs.settimeout(timeout)
         status = cs.connect_ex(address)
         return True if status == 0 else False
+    
+    
+class ProxyServer(object):
+    pass
