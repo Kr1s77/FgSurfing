db05df417b6246e8b6a9aa8a0e9c299b1b69fb42
Learning for learning <1148995537@qq.com>
2021-06-09 16:21:02 +0800

    update device class add aiplane mode and port 👨‍🎓

--- adb.py
+++ adb.py
@@ -30,4 +30,34 @@ class AdbTools(object):
         
      
 class Device(object):
-    pass
+    """Android Device...
+    """
+    def __init__(
+            self,
+            device_id: str,
+            port: int = 8118,
+            adb: AdbTools = AdbTools(),
+            network: Network = Network()
+    ):
+        self.adb = adb
+        self.port = port
+        self.network = network
+        self.device_id = device_id
+        self.airplane_mode_is_open = False  # 飞行模式
+        self.transfer_port_is_open = False  # 手机端口
+        self.device_is_died = False
+        # self.fg_network_is_open = False
+
+        self.init_mobile_settings()  # 初始化手机的信息
+
+    def init_mobile_settings(self):
+        self.airplane_mode_is_open = self.adb.check_airplane_status(
+            device_id=self.device_id
+        )
+
+        self.transfer_port_is_open = self.network.check_local_port(
+            port=self.port
+        )
+
+        if not self.transfer_port_is_open:
+            self.device_is_died = True
