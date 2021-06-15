03530cc0ce06e917b36c79f40471642f31c55d40
Learning for learning <1148995537@qq.com>
2021-06-15 15:43:28 +0800

    add runner test

--- api.py
+++ api.py
@@ -1,1 +1,26 @@
 # _*_ coding: utf-8 _*_
+import sys
+import time
+from adb import Device, AdbTools
+
+
+def runner() -> None:
+    adb = AdbTools()
+    devices = [Device(device, adb=adb) for device in adb.get_remote_devices()]
+    for device in devices:
+        network_on = device.check_4g_network()
+        # 开启代理模式，多进程
+    # message >> 输出出来
+
+    import time
+    import sys
+    count = 1
+    while count < 99:
+        sys.stdout.write("current {0}%\r".format(count))
+    sys.stdout.flush()
+    count += 1
+    time.sleep(0.5)
+
+
+if __name__ == '__main__':
+    runner()
