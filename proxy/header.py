99aa41a133e22e78e972b1c4c699896e2b7669cc
Learning for learning <1148995537@qq.com>
2021-07-12 15:30:31 +0800

    filter http request header

--- proxy/header.py
+++ proxy/header.py
@@ -7,8 +7,30 @@ Use this module to replace the request headers
 """
 
 
-class Header(object):
-    """Replace Request Headers"""
-    def __init__(self, data: bytes):
-        if not data or b'\r\n\r\n' not in data:
-            self._header = None
+import re
+
+
+FILTER_HEADER_SEGMENTS = (
+    'connection',
+    'keep-alive',
+    'proxy-authenticate',
+    'proxy-authorization',
+    'te',
+    'trailers',
+    'transfer-encoding',
+    'upgrade'
+)
+FILTER_HEADER_SEGMENTS_UPPER = tuple([i.upper() for i in FILTER_HEADER_SEGMENTS])
+
+
+def filter_header(headers):
+    for key, upper_key in zip(FILTER_HEADER_SEGMENTS, FILTER_HEADER_SEGMENTS_UPPER):
+        del headers[key]
+        del headers[upper_key]
+
+    if 'Accept-Encoding' in headers:
+        ae = headers['Accept-Encoding']
+        filtered_encodings = [x for x in re.split(r',\s*', ae) if x in ('identity', 'gzip', 'x-gzip', 'deflate')]
+        headers['Accept-Encoding'] = ', '.join(filtered_encodings)
+
+    return headers
