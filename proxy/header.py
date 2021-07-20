028002b43b561c63d3caa16aeec17a3e5d893bed
Learning for learning <1148995537@qq.com>
2021-07-20 16:21:15 +0800

    fix headers

--- proxy/header.py
+++ proxy/header.py
@@ -5,8 +5,6 @@ FgSurfing.api
 Use this module to replace the request headers
 :copyright: (c) 2021 by Kris
 """
-
-
 import re
 
 
@@ -20,13 +18,25 @@ FILTER_HEADER_SEGMENTS = (
     'transfer-encoding',
     'upgrade'
 )
+
+
+def _to_capitalize(word: str):
+    return '-'.join([i.capitalize() for i in word.split('-')])
+
+
 FILTER_HEADER_SEGMENTS_UPPER = tuple([i.upper() for i in FILTER_HEADER_SEGMENTS])
+FILTER_HEADER_SEGMENTS_CAPITALIZE = tuple([_to_capitalize(i) for i in FILTER_HEADER_SEGMENTS])
 
 
 def filter_header(headers):
-    for key, upper_key in zip(FILTER_HEADER_SEGMENTS, FILTER_HEADER_SEGMENTS_UPPER):
+    for key, upper_key, capitalize_key in zip(
+            FILTER_HEADER_SEGMENTS,
+            FILTER_HEADER_SEGMENTS_UPPER,
+            FILTER_HEADER_SEGMENTS_CAPITALIZE
+    ):
         del headers[key]
         del headers[upper_key]
+        del headers[capitalize_key]
 
     if 'Accept-Encoding' in headers:
         ae = headers['Accept-Encoding']
