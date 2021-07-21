905a29e3cedfe8d2049559be8f9ce36586b29b17
Learning for learning <1148995537@qq.com>
2021-07-21 11:17:34 +0800

    add device status

--- proxy/adb.py
+++ proxy/adb.py
@@ -5,20 +5,22 @@ FGSurfing.api
 Use this module connect your mobile by android debug bridge
 :copyright: (c) 2021 by Kris
 """
-import os
 import time
 from network import Network
-from func_timeout import exceptions
-from func_timeout import func_set_timeout
 from command import CmdExecute, AdbCommand
 
 
 class AdbTools(object):
     """Can use adb cmd...
     """
+
     def __init__(self, device_id):
         self.command = AdbCommand(device_id=device_id)
         self.cmd = CmdExecute(self.command)
+
+    def check_airplane_status(self):
+        # :Check airplane mode is open
+        return True if int(self.cmd.execute('CHECK_AIRPLANE_MODE')) else False
 
     def turn_on_airplane_mode(self):
         self.cmd.execute('TURN_ON_AIRPLANE_MODE')
@@ -44,49 +46,49 @@ class AdbTools(object):
     def running_server(self, host, port):
         return self.cmd.execute('RUN_SERVER', host, str(port), '> /dev/null', '&')
 
-    def check_remote_port(self, host, port):
-        return self.cmd.execute('CHECK_PORT', host, str(port))
+    def check_remote_port(self, port: int) -> bool:
+        return True if int(self.cmd.execute('CHECK_PORT', str(port))) else False
 
-    def root(self):
+    def start_as_root(self):
         flag = self.cmd.execute('ROOT')
         time.sleep(2)
         return flag
 
     def kill_port(self, port):
-        process_id = self.cmd.execute('PROCESS_ID', port, '| grep tcp | awk "{print $7}" | awk -F/ "{print $1}"')
-        return self.cmd.execute('KILL_PROCESS', process_id)
+        return self.cmd.execute(
+            'PROCESS_ID',
+            str(port),
+            '| grep -v grep | awk "{print $2}"| sed -e "s/^/kill -9 /g" | sh'
+        )
 
-     
+
 class Device(object):
     """Android Device...
     """
+
     def __init__(
             self,
             device_id: str,
             port: int = 8118,
             ip: str = '0.0.0.0',
-            # network: Network = Network()
+            network: Network = Network()
     ):
         self.ip = ip
         self.port = port
-        # self.network = network
+        self.network = network
         self.device_id = device_id
+        self.adb = AdbTools(device_id=device_id)  # adb
+
         self.airplane_mode_is_open = False  # 飞行模式
         self.transfer_port_is_open = False  # 手机端口
-        self.device_is_died = False
-        self.adb = AdbTools(device_id=device_id)  # adb
-        # self.fg_network_is_open = False
 
-        # self.init_mobile_settings()  # 初始化手机的信息
+        self.initialize_device()  # 初始化手机的信息
 
-    # def init_mobile_settings(self):
-    #     self.airplane_mode_is_open = self.adb.check_airplane_status(
-    #         device_id=self.device_id
-    #     )
-    #
-    #     self.transfer_port_is_open = self.network.check_local_port(
-    #         port=self.port
-    #     )
-    #
-    #     if not self.transfer_port_is_open:
-    #         self.device_is_died = True
+    def initialize_device(self) -> None:
+        self.adb.start_as_root()  # start device as root
+
+        self.airplane_mode_is_open = self.adb.check_airplane_status()
+        self.transfer_port_is_open = self.adb.check_remote_port(
+            port=self.port
+        )
+        return None
