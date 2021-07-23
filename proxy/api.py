81378796bca9427eb2ba9aec9b562ca62c8de626
Learning for learning <1148995537@qq.com>
2021-07-23 13:51:53 +0800

    python api.py

--- proxy/api.py
+++ proxy/api.py
@@ -8,12 +8,17 @@ Why cluster running at ip6-localhost:8899?
 127.0.0.1       localhost
 ::1             ip6-localhost
 
-It can also run in ::1:8899.
+It can also running at ::1:8899.
+
+Next step: Check if 4G is available.
 """
+import time
 import logging
 from adb import Device
 from functools import partial
 from command import CmdExecute
+from multiprocessing import Queue
+from multiprocessing import Process
 from deploy.cli import deploy_to_remote
 from __init__ import __author__, __version__, __site__
 
@@ -23,12 +28,17 @@ log = logging.getLogger(__name__)
 def configure_logging(level):
     logging.basicConfig(
         level=level,
-        format='%(asctime)s [%(levelname)s]     %(message)s',
+        format='[Agent %(asctime)s %(levelname)s]  ->  %(message)s',
     )
 
 
 _set_debug_logging = partial(configure_logging, logging.DEBUG)
 _set_info_logging = partial(configure_logging, logging.INFO)
+
+IP_SWITCHING_TIME = 1 * 60   # second
+MASTER_PORT_START = 30000
+HEALTH_CHECK_TIME = 1        # second
+WAIT_AIRPLANE_MODE_TIME = 8  # second
 
 
 def _init_msg(debug: bool) -> None:
@@ -70,52 +80,146 @@ def init_all_devices():
     return devices
 
 
-def _running_proxy(device: Device):
-    pass
+def _forward_tcp_port(device: Device, local_port, remote_port):
+    # nginx complex balanced local port
+    return device.adb.forward_tcp_port(local_port, remote_port)
 
 
-def _daemon_proxy(device: Device):
-    pass
+def kill_master_port(device: Device, port: int):
+    return device.adb.kill_master_process(port)
 
 
-def runner(debug: bool = True, ip: str = '0.0.0.0', port: int = 30000, open_ssl: bool = True) -> None:
-    """program entry
-     1. deploy the application to the phone.
-     2. get all device and init all device.
-     3. running application and output log.
-     """
-    # init
-    _init_msg(debug=debug)
+def run_server(device, master_port):
+    """
+    if int(device.adb.check_remote_port(ip, port).strip()) != 0:
+    The port is already in use
+    """
+    kill_master_port(device, master_port)
+    device.adb.kill_port_process(device.port)
 
-    # init all devices
-    devices_str = init_all_devices()
+    # :If airplane mode is opened, first need close airplane mode
+    if device.airplane_mode_is_open:
+        device.adb.close_airplane_mode()
+        time.sleep(WAIT_AIRPLANE_MODE_TIME)
 
-    # not have devices exit
-    if not devices_str:
-        log.warning('No connected phones were found')
-        exit(1)
+    # :Running Proxy server command
+    device.adb.running_server(host=device.ip, port=device.port)
+    _forward_tcp_port(device, master_port, device.port)
 
-    # create all device object
-    devices = list()
-    for device_id in devices_str:
-        devices.append(
-            Device(device_id=device_id, port=port, ip=ip)
-        )
 
-    # deploy file and running proxy and create proxy daemon
+def _change_ip(master_port: int, cluster_device: Device, change_ip_queue):
+    """
+    # :Close master port for nginx
+    # :Get device info
+    # :Check proxy running port
+    # :Time to change ip
+    # :Open master port for nginx
+    """
+    change_ip_queue.put_nowait(cluster_device.device_id)
+
+    cluster_device.adb.remove_forward(master_port)
+    cluster_device.adb.kill_port_process(cluster_device.port)
+    cluster_device.initialize_device()
+
+    if not cluster_device.airplane_mode_is_open:
+        cluster_device.adb.turn_on_airplane_mode()
+    cluster_device.adb.close_airplane_mode()
+
+    time.sleep(WAIT_AIRPLANE_MODE_TIME)
+    # if cluster_device.transfer_port_is_open is False:
+    cluster_device.adb.running_server(
+        host=cluster_device.ip,
+        port=cluster_device.port
+    )
+
+    _forward_tcp_port(
+        device=cluster_device,
+        local_port=master_port,
+        remote_port=cluster_device.port
+    )
+
+    change_ip_queue.get_nowait()
+
+    time.sleep(IP_SWITCHING_TIME)
+
+
+def _health_check(master_port: int, device: Device, change_ip_queue):
+    # The device will not pass health check if the IP
+    # is being switched, otherwise problems will occur
+    if not change_ip_queue.empty():
+        if device.device_id == change_ip_queue.get_nowait():
+            change_ip_queue.put_nowait(device.device_id)
+            time.sleep(HEALTH_CHECK_TIME)
+            return None
+
+        change_ip_queue.put_nowait(device.device_id)
+        time.sleep(HEALTH_CHECK_TIME)
+
+    device.initialize_device()
+    if not device.transfer_port_is_open:
+        device.adb.remove_forward(port=master_port)
+
+    if device.airplane_mode_is_open:
+        device.adb.remove_forward(port=master_port)
+
+    time.sleep(HEALTH_CHECK_TIME)
+    return None
+
+
+class Daemon(object):
+    def __init__(self, devices: list):
+        self.devices = devices
+        self.change_ip_queue = Queue()
+
+    def worker(self, work_func, change_ip_queue):
+        while True:
+            for index, device in enumerate(self.devices):
+                master_port = MASTER_PORT_START + index
+                work_func(master_port, device, change_ip_queue)
+
+    def run_forever(self):
+        """Running change ip and health check"""
+        process = [Process(target=self.worker, args=(_change_ip, self.change_ip_queue)),
+                   Process(target=self.worker, args=(_health_check, self.change_ip_queue))]
+
+        [p.start() for p in process]
+        [p.join() for p in process]
+
+
+def _deploy_all_device(devices: list) -> None:
     for index, device in enumerate(devices):
         log.info(f'Deploy device: {device.device_id}  {len(devices)}/{index + 1}')
+        log.info(f'Device: {device.device_id} transfer port running is {device.transfer_port_is_open}')
+        log.info(f'Device: {device.device_id} transfer airplane mode open is {device.airplane_mode_is_open}')
+        # :Deploy and run server...
         deploy_to_remote(device=device)
-        _running_proxy(device=device)
-        _daemon_proxy(device=device)
+        master_port = MASTER_PORT_START + index
+        run_server(device=device, master_port=master_port)
+        time.sleep(5)
 
-    if open_ssl:
-        log.info(f'FGProxy {__version__} remote running at {ip}:{port} openSSL: True')
-    else:
-        log.info(f'FGProxy {__version__} remote running at {ip}:{port} openSSL: False')
+    daemon = Daemon(devices=devices)
+    daemon.run_forever()
 
 
+def runner(debug: bool = True, ip: str = '0.0.0.0', port: int = 30000) -> None:
+    """master entry
+     1. deploy the application to the phone.
+     2. get all device and init all device.
+     3. kill all proxy running port
+     4. running application and output log.
+     """
+    _init_msg(debug=debug)
+    devices_str = init_all_devices()
+
+    if not devices_str:
+        log.warning('No connected mobile phone was found')
+        exit(1)
+
+    devices = [Device(device_id=device_id, port=port, ip=ip) for device_id in devices_str]
+    _deploy_all_device(devices)
+
+    log.info(f'FGProxy {__version__} remote running at {ip}:{port}')
 
 
 if __name__ == '__main__':
-    runner(True, 'ip6-localhost', 8899)
+    runner(True, 'ip6-localhost', 10000)
